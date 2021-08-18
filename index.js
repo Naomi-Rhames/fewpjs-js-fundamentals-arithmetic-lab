8c089a975c9d6a42dcfadca2dfaa39fc1c56c59f
Naomi-Rhames <naomirhames2020@gmail.com>
2021-08-18 13:48:39 -0500

    commit tree fb1dbc31b6b48e03353014691c4a00f65c0ca0f5

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-function math(multiply){
-    
-}
-const multiply = num1 * num2
+// test 1
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2 
+
+
+// test 2
+
+
+const random = Math.floor(Math.random() * 10) + 1 
+
+// test 3
+
+
+const num3 = 8
+const num4 = 2
+const mod = num3 / num4
+
+
+// test 4
+
+const max = Math.max(5, 10, 15, 20);
